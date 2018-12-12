d574bade4757f8a16217748df38eaf0e4d10bb9c
startlifecoin <43876881+startlifecoin@users.noreply.github.com>
2018-12-12 03:52:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -89,10 +89,10 @@ BWKVERSION="1.0.0"
 #!/bin/bash
 
 # Check if we are root
-if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root." 1>&2
-   exit 1
-fi
+#if [ "$(id -u)" != "0" ]; then
+#   echo "This script must be run as root." 1>&2
+#   exit 1
+#fi
 
 # Install tools for dig and systemctl
 echo "Preparing installation..."
