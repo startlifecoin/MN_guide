08b8e8d096e8c987cfd1068f82cc3564af6896f4
startlifecoin <43876881+startlifecoin@users.noreply.github.com>
2018-12-12 02:11:52 +0200

    Update startlife_install.sh

--- startlife_install.sh
+++ startlife_install.sh
@@ -1,248 +1,293 @@
+
 #!/bin/bash
 
-TMP_FOLDER=$(mktemp -d)
-CONFIG_FILE='startlife.conf'
-CONFIGFOLDER='/root/.startlife'
-COIN_DAEMON='startlifed'
-COIN_CLI='startlife-cli'
-COIN_PATH='/usr/local/bin/'
-COIN_TGZ='https://github.com/startlifecoin/startlife/releases/download/1.1.0.0/Wallet-Linux.zip'
-COIN_NAME='startlife'
-COIN_PORT=27018
-RPC_PORT=27019
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
 
-NODEIP=$(curl -s4 icanhazip.com)
+case $key in
+    -a|--advanced)
+    ADVANCED="y"
+    shift
+    ;;
+    -n|--normal)
+    ADVANCED="n"
+    FAIL2BAN="y"
+    UFW="y"
+    BOOTSTRAP="y"
+    shift
+    ;;
+    -i|--externalip)
+    EXTERNALIP="$2"
+    ARGUMENTIP="y"
+    shift
+    shift
+    ;;
+    -k|--privatekey)
+    KEY="$2"
+    shift
+    shift
+    ;;
+    -f|--fail2ban)
+    FAIL2BAN="y"
+    shift
+    ;;
+    --no-fail2ban)
+    FAIL2BAN="n"
+    shift
+    ;;
+    -u|--ufw)
+    UFW="y"
+    shift
+    ;;
+    --no-ufw)
+    UFW="n"
+    shift
+    ;;
+    -b|--bootstrap)
+    BOOTSTRAP="y"
+    shift
+    ;;
+    --no-bootstrap)
+    BOOTSTRAP="n"
+    shift
+    ;;
+    -h|--help)
+    cat << EOL
+SCN Masternode installer arguments:
+    -n --normal               : Run installer in normal mode
+    -a --advanced             : Run installer in advanced mode
+    -i --externalip <address> : Public IP address of VPS
+    -k --privatekey <key>     : Private key to use
+    -f --fail2ban             : Install Fail2Ban
+    --no-fail2ban             : Don't install Fail2Ban
+    -u --ufw                  : Install UFW
+    --no-ufw                  : Don't install UFW
+    -b --bootstrap            : Sync node using Bootstrap
+    --no-bootstrap            : Don't use Bootstrap
+    -h --help                 : Display this help text.
+EOL
+    exit
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
 
+clear
 
-RED='\033[0;31m'
-YELLOW='\033[1;33m'
-GREEN='\033[0;32m'
-NC='\033[0m'
+# Set these to change the version of SecureCloud to install
+TARBALLURL="https://github.com/startlifecoin/startlife/releases/download/1.1.0.0/linux-gnu.tar.gz"
+TARBALLNAME="linux-gnu.tar.gz"
+BOOTSTRAPURL=""
+BOOTSTRAPARCHIVE=""
+BWKVERSION="1.0.0"
 
-function download_node() {
-  echo -e "Downloading and installing latest ${GREEN}$COIN_NAME${NC} coin daemon."
-  cd $TMP_FOLDER >/dev/null 2>&1
-  wget -q $COIN_TGZ -O $COIN_DAEMON.zip --show-progress
-  compile_error
-  unzip -j $COIN_DAEMON.zip >/dev/null 2>&1
-  compile_error
-  rm $COIN_DAEMON.zip
-  chmod +x *
-  cp $COIN_DAEMON $COIN_PATH
-  cp $COIN_CLI $COIN_PATH
-  cd ~ >/dev/null 2>&1
-  rm -rf $TMP_FOLDER >/dev/null 2>&1
-  clear
-}
+#!/bin/bash
 
+# Check if we are root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root." 1>&2
+   exit 1
+fi
 
-function configure_systemd() {
-  cat << EOF > /etc/systemd/system/$COIN_NAME.service
-[Unit]
-Description=$COIN_NAME service
-After=network.target
+# Install tools for dig and systemctl
+echo "Preparing installation..."
+apt-get install git dnsutils systemd -y > /dev/null 2>&1
 
-[Service]
-User=root
-Group=root
+# Check for systemd
+systemctl --version >/dev/null 2>&1 || { echo "systemd is required. Are you using Ubuntu 16.04?"  >&2; exit 1; }
 
-Type=forking
-#PIDFile=$CONFIGFOLDER/$COIN_NAME.pid
+# CHARS is used for the loading animation further down.
+CHARS="/-\|"
+if [ -z "$EXTERNALIP" ]; then
+EXTERNALIP=`dig +short myip.opendns.com @resolver1.opendns.com`
+fi
+clear
 
-ExecStart=$COIN_PATH$COIN_DAEMON -daemon -conf=$CONFIGFOLDER/$CONFIG_FILE -datadir=$CONFIGFOLDER
-ExecStop=-$COIN_PATH$COIN_CLI -conf=$CONFIGFOLDER/$CONFIG_FILE -datadir=$CONFIGFOLDER stop
+if [ -z "$ADVANCED" ]; then
+echo "
+    ___T_
+   | o o |
+   |__-__|
+   /| []|\\
+ ()/|___|\()
+    |_|_|
+    /_|_\  ------- MASTERNODE INSTALLER v2 -------+
+ |                                                  |
+ | You can choose between two installation options: |::
+ |              default and advanced.               |::
+ |                                                  |::
+ |  The advanced installation will install and run  |::
+ |   the masternode under a non-root user. If you   |::
+ |   don't know what that means, use the default    |::
+ |               installation method.               |::
+ |                                                  |::
+ |  Otherwise, your masternode will not work, and   |::
+ | the STLC Team CANNOT assist you in repairing  |::
+ |         it. You will have to start over.         |::
+ |                                                  |::
+ +------------------------------------------------+::
+   ::::::::::::::::::::::::::::::::::::::::::::::::::
+"
 
-Restart=always
-PrivateTmp=true
-TimeoutStopSec=60s
-TimeoutStartSec=10s
-StartLimitInterval=120s
-StartLimitBurst=5
+sleep 5
+fi
 
-[Install]
-WantedBy=multi-user.target
-EOF
+if [ -z "$ADVANCED" ]; then
+read -e -p "Use the Advanced Installation? [N/y] : " ADVANCED
+fi
 
-  systemctl daemon-reload
-  sleep 3
-  systemctl start $COIN_NAME.service
-  systemctl enable $COIN_NAME.service >/dev/null 2>&1
+if [[ ("$ADVANCED" == "y" || "$ADVANCED" == "Y") ]]; then
 
-  if [[ -z "$(ps axo cmd:100 | egrep $COIN_DAEMON)" ]]; then
-    echo -e "${RED}$COIN_NAME is not running${NC}, please investigate. You should start by running the following commands as root:"
-    echo -e "${GREEN}systemctl start $COIN_NAME.service"
-    echo -e "systemctl status $COIN_NAME.service"
-    echo -e "less /var/log/syslog${NC}"
-    exit 1
-  fi
-}
+USER=securecloud
 
+adduser $USER --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password > /dev/null
 
-function create_config() {
-  mkdir $CONFIGFOLDER >/dev/null 2>&1
-  RPCUSER=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w10 | head -n1)
-  RPCPASSWORD=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w22 | head -n1)
-  cat << EOF > $CONFIGFOLDER/$CONFIG_FILE
-rpcuser=$RPCUSER
-rpcpassword=$RPCPASSWORD
-rpcport=$RPC_PORT
+INSTALLERUSED="#Used Advanced Install"
+
+echo "" && echo 'Added user "startlife"' && echo ""
+sleep 1
+
+else
+
+USER=root
+FAIL2BAN="y"
+UFW="y"
+BOOTSTRAP="n"
+INSTALLERUSED="#Used Basic Install"
+fi
+
+USERHOME=`eval echo "~$USER"`
+
+if [ -z "$ARGUMENTIP" ]; then
+read -e -p "Server IP Address: " -i $EXTERNALIP -e IP
+fi
+
+if [ -z "$KEY" ]; then
+read -e -p "Masternode Private Key (e.g. 7edfjLCUzGczZi3JQw8GHp434R9kNY33eFyMGeKRymkB56G4324h # THE KEY YOU GENERATED EARLIER) : " KEY
+fi
+
+if [ -z "$FAIL2BAN" ]; then
+read -e -p "Install Fail2ban? [Y/n] : " FAIL2BAN
+fi
+
+if [ -z "$UFW" ]; then
+read -e -p "Install UFW and configure ports? [Y/n] : " UFW
+fi
+
+if [ -z "$BOOTSTRAP" ]; then
+read -e -p "Do you want to use our bootstrap file to speed the syncing process? [Y/n] : " BOOTSTRAP
+fi
+
+clear
+
+# Generate random passwords
+RPCUSER=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
+RPCPASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+
+# update packages and upgrade Ubuntu
+echo "Installing dependencies..."
+apt-get -qq update
+apt-get -qq upgrade
+apt-get -qq autoremove
+apt-get -qq install wget htop unzip
+apt-get -qq install build-essential && apt-get -qq install libtool autotools-dev autoconf libevent-pthreads-2.0-5 automake && apt-get -qq install libssl-dev && apt-get -qq install libboost-all-dev && apt-get -qq install software-properties-common && add-apt-repository -y ppa:bitcoin/bitcoin && apt update && apt-get -qq install libdb4.8-dev && apt-get -qq install libdb4.8++-dev && apt-get -qq install libminiupnpc-dev && apt-get -qq install libqt4-dev libprotobuf-dev protobuf-compiler && apt-get -qq install libqrencode-dev && apt-get -qq install git && apt-get -qq install pkg-config && apt-get -qq install libzmq3-dev
+apt-get -qq install aptitude
+apt-get -qq install libevent-dev
+
+# Install Fail2Ban
+if [[ ("$FAIL2BAN" == "y" || "$FAIL2BAN" == "Y" || "$FAIL2BAN" == "") ]]; then
+  aptitude -y -q install fail2ban
+  service fail2ban restart
+fi
+
+# Install UFW
+if [[ ("$UFW" == "y" || "$UFW" == "Y" || "$UFW" == "") ]]; then
+  apt-get -qq install ufw
+  ufw default deny incoming
+  ufw default allow outgoing
+  ufw allow ssh
+  ufw allow 9191/tcp
+  yes | ufw enable
+fi
+
+# Install SCN daemon
+wget $TARBALLURL
+tar -xzvf $TARBALLNAME 
+rm $TARBALLNAME
+mv ./startlifed /usr/local/bin
+mv ./startlife-cli /usr/local/bin
+mv ./startlife-tx /usr/local/bin
+rm -rf $TARBALLNAME
+
+# Create .startlife directory
+mkdir $USERHOME/.startlife
+
+# Install bootstrap file
+if [[ ("$BOOTSTRAP" == "y" || "$BOOTSTRAP" == "Y" || "$BOOTSTRAP" == "") ]]; then
+  echo "skipping"
+fi
+
+# Create startlife.conf
+touch $USERHOME/.startlife/startlife.conf
+cat > $USERHOME/.startlife/startlife.conf << EOL
+${INSTALLERUSED}
+rpcuser=${RPCUSER}
+rpcpassword=${RPCPASSWORD}
 rpcallowip=127.0.0.1
 listen=1
 server=1
 daemon=1
-port=$COIN_PORT
-EOF
-}
-
-function create_key() {
-  echo -e "Enter your ${RED}$COIN_NAME Masternode Private Key${NC}. Leave it blank to generate a new ${RED}Masternode Private Key${NC} for you:"
-  read -e COINKEY
-  if [[ -z "$COINKEY" ]]; then
-  $COIN_PATH$COIN_DAEMON -daemon
-  sleep 30
-  if [ -z "$(ps axo cmd:100 | grep $COIN_DAEMON)" ]; then
-   echo -e "${RED}$COIN_NAME server couldn not start. Check /var/log/syslog for errors.{$NC}"
-   exit 1
-  fi
-  COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
-  if [ "$?" -gt "0" ];
-    then
-    echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the Private Key${NC}"
-    sleep 30
-    COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
-  fi
-  $COIN_PATH$COIN_CLI stop
-fi
-clear
-}
-
-function update_config() {
-  cat << EOF >> $CONFIGFOLDER/$CONFIG_FILE
-#bind=$NODEIP
+logtimestamps=1
+maxconnections=256
+externalip=${IP}
+bind=${IP}:27018
+masternodeaddr=${IP}
+masternodeprivkey=${KEY}
 masternode=1
-masternodeaddr=$NODEIP:$COIN_PORT
-masternodeprivkey=$COINKEY
-EOF
-}
+addnode=188.120.227.155
+addnode=188.120.237.71
+addnode=188.120.237.75
+addnode=188.120.234.242
+addnode=188.120.235.175
+EOL
+chmod 0600 $USERHOME/.startlife/startlife.conf
+chown -R $USER:$USER $USERHOME/.startlife
 
+sleep 1
 
-function enable_firewall() {
-  echo -e "Installing and setting up firewall to allow ingress on port ${GREEN}$COIN_PORT${NC}"
-  ufw allow $COIN_PORT/tcp comment "$COIN_NAME MN port" >/dev/null
-  ufw allow ssh comment "SSH" >/dev/null 2>&1
-  ufw limit ssh/tcp >/dev/null 2>&1
-  ufw default allow outgoing >/dev/null 2>&1
-  echo "y" | ufw enable >/dev/null 2>&1
-}
+cat > /etc/systemd/system/startlife.service << EOL
+[Unit]
+Description=startlifed
+After=network.target
+[Service]
+Type=forking
+User=${USER}
+WorkingDirectory=${USERHOME}
+ExecStart=/usr/local/bin/startlifed -conf=${USERHOME}/.startlife/startlife.conf -datadir=${USERHOME}/.startlife
+ExecStop=/usr/local/bin/startlife-cli -conf=${USERHOME}/.startlife/startlife.conf -datadir=${USERHOME}/.startlife stop
+Restart=on-abort
+[Install]
+WantedBy=multi-user.target
+EOL
+sudo systemctl enable startlife.service
+sudo systemctl start startlife.service
 
-
-function get_ip() {
-  declare -a NODE_IPS
-  for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
-  do
-    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
-  done
-
-  if [ ${#NODE_IPS[@]} -gt 1 ]
-    then
-      echo -e "${GREEN}More than one IP. Please type 0 to use the first IP, 1 for the second and so on...${NC}"
-      INDEX=0
-      for ip in "${NODE_IPS[@]}"
-      do
-        echo ${INDEX} $ip
-        let INDEX=${INDEX}+1
-      done
-      read -e choose_ip
-      NODEIP=${NODE_IPS[$choose_ip]}
-  else
-    NODEIP=${NODE_IPS[0]}
-  fi
-}
-
-
-function compile_error() {
-if [ "$?" -gt "0" ];
- then
-  echo -e "${RED}Failed to compile $COIN_NAME. Please investigate.${NC}"
-  exit 1
-fi
-}
-
-
-function checks() {
-if [[ $(lsb_release -d) != *16.04* ]]; then
-  echo -e "${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
-  exit 1
-fi
-
-if [[ $EUID -ne 0 ]]; then
-   echo -e "${RED}$0 must be run as root.${NC}"
-   exit 1
-fi
-
-if [ -n "$(pidof $COIN_DAEMON)" ] || [ -e "$COIN_DAEMON" ] ; then
-  echo -e "${RED}$COIN_NAME is already installed.${NC}"
-  exit 1
-fi
-}
-
-function prepare_system() {
-echo -e "Prepare the system to install ${GREEN}$COIN_NAME${NC} master node."
-apt-get update >/dev/null 2>&1
-DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null 2>&1
-DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade >/dev/null 2>&1
-apt install -y software-properties-common >/dev/null 2>&1
-echo -e "${GREEN}Adding bitcoin PPA repository"
-apt-add-repository -y ppa:bitcoin/bitcoin >/dev/null 2>&1
-echo -e "Installing required packages, it may take some time to finish.${NC}"
-apt-get update >/dev/null 2>&1
-apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
-build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev \
-libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git wget curl libdb4.8-dev bsdmainutils libdb4.8++-dev \
-libminiupnpc-dev libgmp3-dev libzmq3-dev ufw pkg-config libevent-dev mc libdb5.3++ unzip >/dev/null 2>&1
-if [ "$?" -gt "0" ];
-  then
-    echo -e "${RED}Not all required packages were installed properly. Try to install them manually by running the following commands:${NC}\n"
-    echo "apt-get update"
-    echo "apt -y install software-properties-common"
-    echo "apt-add-repository -y ppa:bitcoin/bitcoin"
-    echo "apt-get update"
-    echo "apt install -y make build-essential libtool software-properties-common autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev \
-libboost-program-options-dev libboost-system-dev libzmq3-dev libboost-test-dev libboost-thread-dev sudo automake git curl libdb4.8-dev \
-bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw pkg-config mc libevent-dev libdb5.3++ unzip"
- exit 1
-fi
-clear
-}
-
-function important_information() {
- echo -e "================================================================================================================================"
- echo -e "$COIN_NAME Masternode is up and running listening on port ${YELLOW}$COIN_PORT${NC}."
- echo -e "Configuration file is: ${YELLOW}$CONFIGFOLDER/$CONFIG_FILE${NC}"
- echo -e "Start: ${YELLOW}systemctl start $COIN_NAME.service${NC}"
- echo -e "Stop: ${YELLOW}systemctl stop $COIN_NAME.service${NC}"
- echo -e "VPS_IP:PORT ${YELLOW}$NODEIP:$COIN_PORT${NC}"
- echo -e "MASTERNODE PRIVATEKEY is: ${YELLOW}$COINKEY${NC}"
- echo -e "Please check ${YELLOW}$COIN_NAME${NC} daemon is running with the following command: ${YELLOW}systemctl status $COIN_NAME.service${NC}"
- echo -e "Use ${YELLOW}$COIN_CLI masternode status${NC} to check your MN. A running MN will show ${YELLOW}Status 9${NC}."
- echo -e "================================================================================================================================"
-}
-
-function setup_node() {
-  get_ip
-  create_config
-  create_key
-  update_config
-  enable_firewall
-  important_information
-  configure_systemd
-}
-
-
-##### Main #####
 clear
 
-checks
-prepare_system
-download_node
-setup_node
+cat << EOL
+Now, you need to start your masternode. Please go to your desktop wallet
+Click the Masternodes tab
+Click Start all at the bottom 
+EOL
+
+read -p "Press Enter to continue after you've done that. " -n1 -s
+
+clear
+
+echo "" && echo "Masternode setup completed." && echo ""
