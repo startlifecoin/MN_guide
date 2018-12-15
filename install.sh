6bd1544bf64103b05b08264cea4a61ac2585a988
startlifecoin <43876881+startlifecoin@users.noreply.github.com>
2018-12-15 10:21:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -80,8 +80,8 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 clear
 
 # Set these to change the version of SecureCloud to install
-TARBALLURL="https://github.com/startlifecoin/startlife/releases/download/1.1.0.0(new)/linux-gnu.tar.gz"
-TARBALLNAME="linux-gnu.tar.gz"
+TARBALLURL="https://github.com/startlifecoin/startlife/releases/download/1.2.0.0/linux-gnu_1.2.0.0.tar.gz"
+TARBALLNAME="linux-gnu_1.2.0.0.tar.gz"
 BOOTSTRAPURL=""
 BOOTSTRAPARCHIVE=""
 BWKVERSION="1.1.0"
